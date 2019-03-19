6b9112de86de8d5351a4f00488acce41769af547
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-03-19 22:36:25 +0200

    Create Tree.cpp

---
+++ Tree.cpp
@@ -0,0 +1,43 @@
+#include "Tree.hpp"
+using namespace ariel;
+Tree::Tree()
+{
+    Troot=NULL;
+}
+Tree::Tree(Node n)
+{
+    Troot=&n;
+}
+  int Tree:: size()
+ {
+    return 0;
+}
+    int Tree:: parent(int a)
+    {
+        return 0;
+    }
+    int Tree:: left(int a)
+    {
+        return 0;
+    }
+    int Tree:: right(int a)
+    {
+        return 0;
+    }
+    Tree& Tree:: insert (int a)
+    {
+        
+    }
+    int Tree:: root()
+    {
+        return 0;
+    }
+    void Tree:: remove (int a){
+        return;
+    }
+    bool Tree:: contains(int a){
+        return false;
+    }
+    void Tree:: print(){
+        return;
+    }
